6f2e8b0765f016d6a6571090f986456c026ff582
William Denton <wtd@pobox.com>
2016-06-08 13:32:04 -0400

    We're going to start this thing off right

---
+++ noa2co2.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# coding: utf-8
+
+require 'csv'
+require 'nokogiri'
+require 'open-uri'
+
+csv_file = "noaa-co2.csv"
+
+csv_data = CSV.read(csv_file)
+
+doc = Nokogiri::HTML(open("http://www.esrl.noaa.gov/gmd/ccgg/trends/monthly.html"))
+doc.css('//.colored_box/table/tr').each do |t|
+  raw_date = t.css('td')[0].text.gsub(" ", '').strip.gsub(":", '') # There's an &nbsp; in the HTML
+  current_year = Time.now().strftime("%Y")
+  date = Date.parse(raw_date + " " + current_year).to_s
+  next if csv_data.flatten.include?(date)
+
+  raw_ppm = t.css('td')[1].text.strip.gsub(" ppm", '')
+  ppm = case
+        when /unavailable/i.match(raw_ppm) then "NA"
+        else
+          raw_ppm
+        end
+  csv_data << [date, ppm]
+end
+
+csv_data.each do |date, ppm|
+  puts [date, ppm].to_csv
+end
