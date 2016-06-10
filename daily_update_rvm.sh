c37ab9124f735e7d125e455c2f0097b56bac6539
William Denton <wtd@pobox.com>
2016-06-10 10:41:32 -0400

    Add cronnable RVM-aware daily update script

--- daily_update_rvm.sh
+++ daily_update_rvm.sh
@@ -3,4 +3,9 @@
 RVM_PATH=`rvm env --path -- ruby-version[@gemset-name]`
 source $RVM_PATH
 
-ruby ./noa₂co₂.rb
+CSV_FILE=mauna-loa-co₂.csv
+
+ruby ./noa₂co₂.rb > tmp.csv
+mv tmp.csv $CSV_FILE
+git add $CSV_FILE
+git commit -m "Daily update"
