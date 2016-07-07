ad3f7581bc40d1ed26c6c27d3f4802037f4ddd13
William Denton <wtd@pobox.com>
2016-07-07 14:34:30 -0400

    Tidying shell scripts

--- rvm_noa₂co₂.sh
+++ rvm_noa₂co₂.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-#PATH=~/.rvm/bin/rvm:$PATH
-
 RVM_PATH=`~/.rvm/bin/rvm env --path -- ruby-version[@gemset-name]`
 source $RVM_PATH
 
-./daily_update.sh
+ruby ./noa₂co₂.rb
--- update_daily.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-ruby ./noa₂co₂.rb
