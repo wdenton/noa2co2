de49f9e1e834c06ad1576e098c422f20aa1be9ed
William Denton <wtd@pobox.com>
2016-07-07 07:33:44 -0400

    Fix RVM path again

--- rvm_daily_update.sh
+++ rvm_daily_update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-PATH=~/.rvm/bin/rvm:$PATH
+#PATH=~/.rvm/bin/rvm:$PATH
 
-RVM_PATH=`rvm env --path -- ruby-version[@gemset-name]`
+RVM_PATH=`~/.rvm/bin/rvm env --path -- ruby-version[@gemset-name]`
 source $RVM_PATH
 
 ./daily_update.sh
