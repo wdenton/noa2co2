7c431aa36a1eab35ef6d090c7907d773fe6821bf
William Denton <wtd@pobox.com>
2016-07-07 14:00:27 -0400

    JSON_FILE needs commitment too

--- daily_update.sh
+++ daily_update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
 CSV_FILE=mauna-loa-co₂.csv
+JSON_FILE=mauna-loa-co₂-latest.json
 
 ruby ./noa₂co₂.rb > tmp.csv
 mv tmp.csv $CSV_FILE
-git add $CSV_FILE
+git add $CSV_FILE $JSON_FILE
 git commit -m "Daily update"
 git push
