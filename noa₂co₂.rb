e2f67ecaeb60b34efdf9eb9604001c8a754fcd50
William Denton <wtd@pobox.com>
2016-12-21 21:47:01 -0500

    Use HTTPS URL; catch up on data
    A few days are missing :(

--- noa₂co₂.rb
+++ noa₂co₂.rb
@@ -9,7 +9,7 @@ require 'open-uri'
 csv_file = 'mauna-loa.csv'
 json_file = 'mauna-loa-latest.json'
 
-mauna_loa_uri = 'http://www.esrl.noaa.gov/gmd/ccgg/trends/monthly.html'
+mauna_loa_uri = 'https://www.esrl.noaa.gov/gmd/ccgg/trends/monthly.html'
 
 csv_data = CSV.read(csv_file)
 
